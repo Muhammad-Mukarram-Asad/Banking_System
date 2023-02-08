0a7c19f2815d1580735b08d0af5ea0fc60a3f8ed
Mukarram Asad <76425569+Muhammad-Mukarram-Asad@users.noreply.github.com>
2023-02-08 23:55:47 +0500

    Delete router_index.js

--- router_index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/routes', require('./routes'), (req,res) => {
-    res.send("routes.js file initialized...");
-});
-
-module.exports = router;
